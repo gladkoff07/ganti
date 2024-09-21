032aafe935fd57ad6430b53a9f357e25c23e1e77
gladkoff07 <t-orange@yandex.ru>
2024-09-21 10:49:12 +0300

    add user authorization nav

--- js/common.js
+++ js/common.js
@@ -8,4 +8,10 @@ document.addEventListener('DOMContentLoaded', function () {
       this.closest('.list-faq__item').querySelector('.list-faq__body').classList.toggle('list-faq__body--active');
     });
   });
+
+  // .js-change-authorization-nav
+  document.querySelector('.js-change-authorization-nav').addEventListener('click', function () {
+    this.classList.toggle('link-authorization--active');
+    this.querySelector('.link-authorization__box').classList.toggle('link-authorization__box--active');
+  });
 });

66761943d2494702bdfae8dafdedff488efe76af
gladkoff07 <t-orange@yandex.ru>
2024-09-21 11:24:51 +0300

    add list-catalog-nav

--- js/common.js
+++ js/common.js
@@ -14,4 +14,11 @@ document.addEventListener('DOMContentLoaded', function () {
     this.classList.toggle('link-authorization--active');
     this.querySelector('.link-authorization__box').classList.toggle('link-authorization__box--active');
   });
+
+  // js-change-list-catalog-nav
+  document.querySelector('.js-change-list-catalog-nav').addEventListener('click', function (e) {
+    e.preventDefault();
+    this.classList.toggle('link-button--active');
+    this.nextElementSibling.classList.toggle('header__link-catalog-box--active');
+  });
 });
